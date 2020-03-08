d2a21870dac5833589c0f15a5a46447402c30b43
KinKam Zhao <kamzh88@hotmail.com>
2020-03-08 13:08:55 -0400

    change position of title

---
+++ client/build/precache-manifest.5a2c0617d0b4cc4f4f57aac9358198e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f197fac9364d808184a4f3c6477542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b5818d9dd7d95316f14",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2bb368dd159d779611da",
+    "url": "/static/css/main.59fbd62f.chunk.css"
+  },
+  {
+    "revision": "3b5818d9dd7d95316f14",
+    "url": "/static/js/2.af58a211.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.af58a211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb368dd159d779611da",
+    "url": "/static/js/main.bf2c33e2.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "b6b9df2c3fde3efd6ab83de01ad1a271",
+    "url": "/static/media/luca-bravo-_QdFx92MO2U-unsplash.b6b9df2c.jpg"
+  },
+  {
+    "revision": "a87d4194c099aa8e32c823408ad2467c",
+    "url": "/static/media/profile-pic.a87d4194.jpg"
+  }
+]);
