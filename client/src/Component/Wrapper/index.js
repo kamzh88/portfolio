ab420bc385bb56d227ce7c7f8e9dc318d933987f
KinKam Zhao <kamzh88@hotmail.com>
2020-03-10 18:15:15 -0400

    projects

--- client/src/Component/Wrapper/index.js
+++ client/src/Component/Wrapper/index.js
@@ -1,33 +1,29 @@
-import React, { Component, Fragment } from 'react';
-import { Card, Container, Row } from 'react-bootstrap';
-import './style.css';
+import React, { Component, Fragment } from "react";
+import { Card, Container, Row } from "react-bootstrap";
+import "./style.css";
 
 const styles = {
-    heading: {
-        fontWeight: 900,
-        fontSize: "2rem"
-    }
-}
+  heading: {
+    fontWeight: 900,
+    fontSize: "2rem"
+  }
+};
 
 class Wrapper extends Component {
-
-    render() {
-
-        return (
-            <Fragment>
-                <Container fluid>
-                    <Row className="justify-content-md-center">
-                        <Card className="text-center">
-                            <Card.Header style={styles.heading}>Kin Kam Zhao</Card.Header>
-                            <Card.Body>
-                               {this.props.children}
-                            </Card.Body>
-                        </Card>
-                    </Row>
-                </Container>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <Container fluid>
+          <Row className="justify-content-md-center">
+            <Card className="text-center">
+              <Card.Header style={styles.heading}>Kin Kam Zhao</Card.Header>
+              <Card.Body>{this.props.children}</Card.Body>
+            </Card>
+          </Row>
+        </Container>
+      </Fragment>
+    );
+  }
 }
 
 export default Wrapper;
--- client/src/Pages/Portfolio/index.js
+++ client/src/Pages/Portfolio/index.js
@@ -1,15 +1,55 @@
-import React, { Component, Fragment } from 'react';
-import Wrapper from '../../Component/Wrapper';
-import './style.css';
+import React, { Component, Fragment } from "react";
+import Wrapper from "../../Component/Wrapper";
+import data from "../../Images/data.json";
+import { Typography } from "@material-ui/core/";
+import "./style.css";
+
+const styles = {
+  card: {},
+  text: {
+    fontWeight: "bold"
+  }
+};
 
 class Portfolio extends Component {
+  state = {
+    data: data
+  };
 
   render() {
+    const { data } = this.state;
 
     return (
       <Fragment>
         <Wrapper>
-
+          <div style={styles.Card}>
+            <Typography variant="h6">Portfolio</Typography>
+            {data.map((project, index) => (
+              <div
+                key={index}
+                style={{
+                  width: "auto",
+                  margin: 20
+                }}
+              >
+                <div>{project.name}</div>
+                <div>{project.summary}</div>
+                <div>
+                  {project.tools ? (
+                    <p>
+                      <span style={styles.text}>Tools Used:</span>
+                      {project.tools}
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div>
+                  <a href={project.url}>See Project</a>
+                </div>
+              </div>
+            ))}
+          </div>
         </Wrapper>
       </Fragment>
     );
