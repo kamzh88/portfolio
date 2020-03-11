3dbf8926c2f4b2924741ab63958e9a1b2f18f3a1
KinKam Zhao <kamzh88@hotmail.com>
2020-03-10 21:40:15 -0400

    styling

--- client/src/Pages/Portfolio/index.js
+++ client/src/Pages/Portfolio/index.js
@@ -1,12 +1,21 @@
 import React, { Component, Fragment } from "react";
 import Wrapper from "../../Component/Wrapper";
 import data from "../../Images/data.json";
-import { Typography } from "@material-ui/core/";
+import { Typography, Card } from "@material-ui/core/";
 import "./style.css";
 
 const styles = {
-  card: {},
+  card: {
+    backgroundColor: `rgba(255, 255, 255, 0.55)`,
+    width: "auto",
+    margin: 5,
+    textAlign: "justify",
+    padding: "1rem"
+  },
   text: {
+    fontWeight: "bold"
+  },
+  heading: {
     fontWeight: "bold"
   }
 };
@@ -22,34 +31,37 @@ class Portfolio extends Component {
     return (
       <Fragment>
         <Wrapper>
-          <div style={styles.Card}>
-            <Typography variant="h6">Portfolio</Typography>
-            {data.map((project, index) => (
-              <div
-                key={index}
-                style={{
-                  width: "auto",
-                  margin: 20
-                }}
-              >
-                <div>{project.name}</div>
-                <div>{project.summary}</div>
-                <div>
-                  {project.tools ? (
-                    <p>
-                      <span style={styles.text}>Tools Used:</span>
-                      {project.tools}
-                    </p>
-                  ) : (
-                    ""
-                  )}
-                </div>
-                <div>
-                  <a href={project.url}>See Project</a>
-                </div>
+          <Typography variant="h6">Portfolio</Typography>
+
+          {data.map((project, index) => (
+            <Card key={index} style={styles.card}>
+              <div style={styles.heading}>{project.name}</div>
+              <br></br>
+              <div>
+                {project.summary ? (
+                  <p>
+                    <span style={styles.text}>Summary:</span><br></br>
+                    {project.summary}
+                  </p>
+                ) : (
+                  ""
+                )}
               </div>
-            ))}
-          </div>
+              <div>
+                {project.tools ? (
+                  <p>
+                    <span style={styles.text}>Tools Used: </span> <br></br>
+                    {project.tools}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <a href={project.url}>See Project</a>
+              </div>
+            </Card>
+          ))}
         </Wrapper>
       </Fragment>
     );
