69609e8cc2e18d2cf8dda478e2e2ae7d0b97faed
KinKam Zhao <kamzh88@hotmail.com>
2020-03-18 13:37:45 -0400

    make page responsive

--- client/src/Component/ProjectInfo/projectInfo.component.jsx
+++ client/src/Component/ProjectInfo/projectInfo.component.jsx
@@ -1,20 +1,6 @@
 import React, { Component, Fragment } from "react";
 import "./projectInfo.styles.scss";
 
-// const styles = {
-//   card: {
-//     padding: "10px",
-//     border: "1px solid grey",
-//     borderRadius: "1px"
-//   },
-//   text: {
-//     fontWeight: "bold"
-//   },
-//   heading: {
-//     fontWeight: "bold"
-//   }
-// };
-
 const ProejctInfo = ({ project }) => (
   <div className="content">
     <h1 className="title">{project.name}</h1>
@@ -53,57 +39,3 @@ const ProejctInfo = ({ project }) => (
 );
 
 export default ProejctInfo;
-
-// const styles = {
-//   card: {
-//     padding: "10px",
-//     border: "1px solid grey",
-//     borderRadius: "1px",
-//   },
-//   text: {
-//     fontWeight: "bold"
-//   },
-//   heading: {
-//     fontWeight: "bold"
-//   }
-// };
-
-// class ProjectInfo extends Component {
-//   render() {
-
-//     return (
-//       <Fragment>
-//         <div style={styles.card}>
-//           <div style={styles.heading}>{this.props.data.name}</div>
-//           <br></br>
-//           <div>
-//             {this.props.data.summary ? (
-//               <p>
-//                 <span style={styles.text}>Summary:</span>
-//                 <br></br>
-//                 {this.props.data.summary}
-//               </p>
-//             ) : (
-//               ""
-//             )}
-//           </div>
-//           <div>
-//             {this.props.data.tools ? (
-//               <p>
-//                 <span style={styles.text}>Tools Used: </span> <br></br>
-//                 {this.props.data.tools}
-//               </p>
-//             ) : (
-//               ""
-//             )}
-//           </div>
-//           <div>
-//             <a href={this.props.data.url}>See Project</a>
-//           </div>
-//         </div>
-//       </Fragment>
-//     );
-//   }
-// }
-
-// export default ProjectInfo;
