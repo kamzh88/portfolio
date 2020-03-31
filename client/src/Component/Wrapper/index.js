11c5512b2410430e0010820dd7be3606125bc7ab
KinKam Zhao <kamzh88@hotmail.com>
2020-03-31 15:08:07 -0400

    change class component to functional component

--- client/src/Component/Wrapper/index.js
+++ client/src/Component/Wrapper/index.js
@@ -2,22 +2,17 @@ import React, { Component, Fragment } from "react";
 import { Card, Container, Row } from "react-bootstrap";
 import "./style.css";
 
-class Wrapper extends Component {
-  render() {
-
-    return (
-      <Fragment>
-        <Container fluid>
-          <Row className="justify-content-md-center">
-            <Card className="text-center" >
-              <Card.Header className="heading">{this.props.title}</Card.Header>
-              <Card.Body className="card-body" >{this.props.children}</Card.Body>
-            </Card>
-          </Row>
-        </Container>
-      </Fragment>
-    );
-  }
-}
+const Wrapper = () => (
+  <Fragment>
+    <Container fluid>
+      <Row className="justify-content-md-center">
+        <Card className="text-center">
+          <Card.Header className="heading">{this.props.title}</Card.Header>
+          <Card.Body className="card-body">{this.props.children}</Card.Body>
+        </Card>
+      </Row>
+    </Container>
+  </Fragment>
+);
 
 export default Wrapper;
