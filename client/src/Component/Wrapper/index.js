3bad2fa96d4abf76d67b501e5c98152428bb683c
KinKam Zhao <kamzh88@hotmail.com>
2020-03-31 15:16:08 -0400

    updates

--- client/src/Component/Wrapper/index.js
+++ client/src/Component/Wrapper/index.js
@@ -2,13 +2,13 @@ import React, { Component, Fragment } from "react";
 import { Card, Container, Row } from "react-bootstrap";
 import "./style.css";
 
-const Wrapper = () => (
+const Wrapper = (props) => (
   <Fragment>
     <Container fluid>
       <Row className="justify-content-md-center">
         <Card className="text-center">
-          <Card.Header className="heading">{this.props.title}</Card.Header>
-          <Card.Body className="card-body">{this.props.children}</Card.Body>
+          <Card.Header className="heading">{props.title}</Card.Header>
+          <Card.Body className="card-body">{props.children}</Card.Body>
         </Card>
       </Row>
     </Container>
