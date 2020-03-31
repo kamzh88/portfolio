b248e00405161ba58acdf5baf06f30f96e7f204c
KinKam Zhao <kamzh88@hotmail.com>
2020-03-31 15:06:16 -0400

    delete comment out info

--- client/src/Pages/Contact/index.js
+++ client/src/Pages/Contact/index.js
@@ -6,16 +6,6 @@ import EmailIcon from "@material-ui/icons/Email";
 import "./style.css";
 
 const styles = {
-  // text: {
-  //   marginBottom: 10
-  // },
-  // form: {
-  //   display: "flex",
-  //   flexDirection: "column",
-  //   maxWidth: 400,
-  //   margin: "auto",
-  //   padding: 16
-  // },
   linkedinIcon: {
     fontSize: 60,
     margin: 20,
@@ -34,18 +24,6 @@ const styles = {
 };
 
 const Contact = () => (
-  // state = {
-  //   name: "",
-  //   email: "",
-  //   message: ""
-  // };
-
-  // handleChange = key => e => {
-  //   this.setState({ [key]: e.target.value });
-  // };
-
-  // const { name, email, message } = this.state;
-
   <Fragment>
     <Wrapper title="Contact Information">
       <div>
@@ -63,69 +41,6 @@ const Contact = () => (
           <GitHubIcon style={styles.githubIcon} />
         </a>
       </div>
-
-      {/* <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Typography variant="h5">
-                Contact Me
-              </Typography>
-            </Grid>
-            <Grid item xs={5}>
-              <Typography variant="h6">
-                Contact Infomation:
-              </Typography>
-              <div>
-                <h7>Phone: (862)208-2672 </h7><br></br>
-                <h7>Email: kamzh1988@gmail.com</h7><br></br>
-                <h7>LinkedIn: </h7><br></br>
-                <h7>Github: </h7><br></br>
-              </div>
-            </Grid>
-            <Grid item xs={7}>
-              <form
-                style={styles.form}
-                onSubmit={this.onSubmit}
-                xs={6}
-              >
-                <TextField
-                  style={styles.text}
-                  variant={"outlined"}
-                  required
-                  type={"text"}
-                  label={"Name"}
-                  value={name}
-                  onChange={this.handleChange("name")}
-                />
-                <TextField
-                  style={styles.text}
-                  variant={"outlined"}
-                  required
-                  type={"email"}
-                  label={"Email"}
-                  value={email}
-                  onChange={this.handleChange("email")}
-                />
-                <TextField
-                  style={styles.text}
-                  required
-                  id="outlined-multiline-static"
-                  label="Message"
-                  multiline
-                  rows="4"
-                  value={message}
-                  variant="outlined"
-                />
-                <Button
-                  type={"submit"}
-                  fullWidth
-                  variant={"contained"}
-                  color={"primary"}
-                >
-                  Send
-            </Button>
-              </form>
-            </Grid>
-          </Grid> */}
     </Wrapper>
   </Fragment>
 );
